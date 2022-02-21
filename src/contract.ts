a42031dc0cd94f6c22b1e001fdacd999614c2612
Syed Jafri <syedjafri97@gmail.com>
2022-02-21 03:11:45 -0700

    Initial commit

---
+++ src/contract.ts
@@ -0,0 +1,41 @@
+import {
+  Name,
+  Action,
+  PermissionLevel,
+  printString,
+  action,
+  contract,
+  packer
+} from "@jafri/as-chain";
+
+@packer
+class MyData {
+  constructor(public name: string) {}
+}
+
+@contract("hello")
+class MyContract {
+  constructor(
+    public receiver: Name,
+    public firstReceiver: Name,
+    public action: Name
+  ) {}
+
+  @action("saygoodbye")
+  sayGoodbye(name: string): void {
+    printString(`+++goodbye, ${name}\n`);
+  }
+
+  @action("sayhello")
+  sayHello(name: string): void {
+    let hello = new MyData("alice");
+    let a = new Action(
+      [new PermissionLevel(this.receiver, Name.fromString("active"))],
+      this.receiver,
+      Name.fromString("saygoodbye"),
+      hello.pack()
+    );
+    a.send();
+    printString(`hello, ${name}\n`);
+  }
+}
